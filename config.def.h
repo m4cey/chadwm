1793596a5a88c0fae9d19413b81cc268d19d51eb
m4cey <h4mmer7ime@gmail.com>
2023-12-21 10:50:42 +0100

    bug annotation on config.def.h

--- config.def.h
+++ config.def.h
@@ -4,44 +4,44 @@
 #include <X11/XF86keysym.h>
 
 /* layout */
-static float mfact                  = 0.50; /* factor of master area size [0.05..0.95] */
-static int nmaster                  = 1;    /* number of clients in master area */
-static int resizehints              = 0;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen     = 0; /* 1 will force focus on the fullscreen window */
+static float mfact                       = 0.50; /* factor of master area size [0.05..0.95] */
+static int nmaster                       = 1;    /* number of clients in master area */
+static int resizehints                   = 0;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen          = 0;    /* 1 will force focus on the fullscreen window */
 
 /* appearance */
-static unsigned int borderpx        = 2;        /* border pixel of windows */
-static const unsigned int default_border = 2;   /* to switch back to default border after dynamic border resizing via keybinds */
-static unsigned int snap            = 16;       /* snap pixel */
-static const unsigned int gappih    = 16;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 16;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 16;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 16;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static unsigned int borderpx             = 2;    /* border pixel of windows */
+static const unsigned int default_border = 2;    /* to switch back to default border after dynamic border resizing via keybinds */
+static unsigned int snap                 = 16;   /* snap pixel */
+static const unsigned int gappih         = 16;   /* horiz inner gap between windows */
+static const unsigned int gappiv         = 16;   /* vert inner gap between windows */
+static const unsigned int gappoh         = 16;   /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov         = 16;   /* vert outer gap between windows and screen edge */
+static const int smartgaps               = 0;    /* 1 means no outer gap when there is only one window */
 static const int unsigned textsystray    = 1;
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails,display systray on the 1st monitor,False: display systray on last monitor*/
-static const int showsystray        = 1;        /* 0 means no systray */
-static int showbar                  = 1;        /* 0 means no bar */
-static const int showtab            = 0; //showtab_auto;
-static int toptab                   = 0;        /* 0 means bottom tab */
-static const int floatbar           = 1;/* 1 means the bar will float(don't have padding),0 means the bar have padding */
-static int topbar                   = 0;        /* 0 means bottom bar */
-static const int horizpadbar        = 0;
-static const int vertpadbar         = 0;
-static const int vertpadtab         = 0;
-static const int horizpadtabi       = 16;
-static const int horizpadtabo       = 16;
-static const int scalepreview       = 4;
-static const int tag_preview        = 0;        /* 1 means enable, 0 is off */
-static const int colorfultag        = 1;        /* 0 means use SchemeSel for selected non vacant tag */
+static const unsigned int systraypinning = 0;    /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;    /* systray spacing */
+static const int systraypinningfailfirst = 1;    /* 1: if pinning fails,display systray on the 1st monitor,False: display systray on last monitor*/
+static const int showsystray             = 1;    /* 0 means no systray */
+static int showbar                       = 1;    /* 0 means no bar */
+static const int showtab                 = 0;    //showtab_auto;
+static int toptab                        = 0;    /* 0 means bottom tab */
+static const int floatbar                = 1;    /* 1 means the bar will float(don't have padding),0 means the bar have padding */
+static int topbar                        = 0;    /* 0 means bottom bar */
+static const int horizpadbar             = 0;
+static const int vertpadbar              = 0;
+static const int vertpadtab              = 1;    // FIXME: crashes if 0
+static const int horizpadtabi            = 0;
+static const int horizpadtabo            = 0;
+static const int scalepreview            = 4;
+static const int tag_preview             = 0;    /* 1 means enable, 0 is off */
+static const int colorfultag             = 1;    /* 0 means use SchemeSel for selected non vacant tag */
+
 static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
 static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
 static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
 static const char *light_up[] = {"xbacklight", "-inc", "5", NULL};
 static const char *light_down[] = {"xbacklight", "-dec", "5", NULL};
-
 #define TERMINAL "st"
 static const char *terminal[] = {TERMINAL, NULL};
 static const char *browser[] = {"firefox", NULL};
@@ -55,7 +55,6 @@ static char primary_font[] = "JetBrainsMono Nerd Font Mono:style:medium:size=12"
 static const char *fonts[]          = { primary_font, "Iosevka:style:medium:size=12" };
 
 // theme
-
 /*
  * Xresources preferences to load at startup
  */
